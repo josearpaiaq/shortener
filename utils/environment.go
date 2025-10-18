965d51db4cb6c77e639ede73bccaed861c2f044f
@josearpaiaq <josearpaiaquintero@gmail.com>
2025-10-18 14:54:10 -0500

    Refactor GET_ENV function to comment out dotenv loading logic

--- utils/environment.go
+++ utils/environment.go
@@ -1,22 +1,25 @@
 package utils
 
 import (
-	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func GET_ENV(key string, defaultValue string) string {
 
-  // load .env file
-  err := godotenv.Load(".env")
+  // // load .env file
+  // err := godotenv.Load(".env")
 
-  if err != nil {
-    log.Fatalf("Error loading .env file")
-  }
+  // if err != nil {
+  //   log.Fatalf("Error loading .env file")
+  // }
 
-  if value, ok := os.LookupEnv(key); ok {
+  // if value, ok := os.LookupEnv(key); ok {
+  //   return value
+  // }
+
+  value := os.Getenv(key)
+
+  if value != "" {
     return value
   }
 
